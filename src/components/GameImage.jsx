6bb34b9999687c618ad21285e471efc9f52f0280
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-31 06:30:56 -0500

    Fix issue with character location
    Click location would change depending on the size of the image.

--- src/components/GameImage.jsx
+++ src/components/GameImage.jsx
@@ -1,5 +1,4 @@
-// /src/components/GameImage.jsx
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import DropDownMenu from './DropDownMenu';
 import styles from './GameImage.module.css';
@@ -12,16 +11,20 @@ const GameImage = ({ onCharacterFound, foundCharacters }) => {
   const [dropDownPosition, setDropDownPosition] = useState({ top: 0, left: 0 });
   const [targetBoxPosition, setTargetBoxPosition] = useState({ top: 0, left: 0 });
   const [clickLocation, setClickLocation] = useState({ x: 0, y: 0 });
+  const imgRef = useRef(null);
 
   const menuWidth = 100; // Width of the dropdown menu
-  const menuHeight = 100; // Height of the dropdown menu
+  const menuHeight = 130; // Height of the dropdown menu
 
   const handleImageClick = (event) => {
-    const imgRect = event.target.getBoundingClientRect();
+    const img = imgRef.current;
+    const imgRect = img.getBoundingClientRect();
+    const xRatio = img.naturalWidth / imgRect.width;
+    const yRatio = img.naturalHeight / imgRect.height;
 
     setClickLocation({
-      x: event.clientX - imgRect.left,
-      y: event.clientY - imgRect.top,
+      x: (event.clientX - imgRect.left) * xRatio,
+      y: (event.clientY - imgRect.top) * yRatio,
     });
 
     setDisplay(!display);
@@ -34,20 +37,22 @@ const GameImage = ({ onCharacterFound, foundCharacters }) => {
       top: event.clientY - imgRect.top - 17, // Center the circle (17px = 34px/2)
       left: event.clientX - imgRect.left - 17, // Center the circle (17px = 34px/2)
     });
+
+    console.log(`x: ${clickLocation.x}, y: ${clickLocation.y}`);
   };
 
   const handleCharacterSelect = async (character) => {
     const characterSelected = await fetchCharacterByName(character);
     // Check that click location is within 15px of the stored character's location
     if (
-      clickLocation.x >= characterSelected.coordinates.x - 15 &&
-      clickLocation.x <= characterSelected.coordinates.x + 15 &&
-      clickLocation.y >= characterSelected.coordinates.y - 15 &&
-      clickLocation.y <= characterSelected.coordinates.y + 15
+      clickLocation.x >= characterSelected.coordinates.x - 30 &&
+      clickLocation.x <= characterSelected.coordinates.x + 30 &&
+      clickLocation.y >= characterSelected.coordinates.y - 30 &&
+      clickLocation.y <= characterSelected.coordinates.y + 30
     ) {
       onCharacterFound(character, {
-        top: characterSelected.coordinates.y - 40,
-        left: characterSelected.coordinates.x - 15,
+        top: characterSelected.coordinates.y,
+        left: characterSelected.coordinates.x,
       });
       console.log(`Found: ${character}`);
     } else {
@@ -59,6 +64,7 @@ const GameImage = ({ onCharacterFound, foundCharacters }) => {
   return (
     <div className={styles.container}>
       <img
+        ref={imgRef}
         onClick={handleImageClick}
         src="src/assets/whereswaldo.jpg"
         alt="where's Waldo game"
@@ -72,9 +78,19 @@ const GameImage = ({ onCharacterFound, foundCharacters }) => {
         handleCharacterSelect={handleCharacterSelect}
       />
       <TargetingBox display={display} position={targetBoxPosition} />
-      {foundCharacters.map((character, index) => (
-        <FoundMarker key={index} found={true} position={character.position} />
-      ))}
+      {foundCharacters.map((character, index) => {
+        const img = imgRef.current;
+        const imgRect = img.getBoundingClientRect();
+        const xRatio = imgRect.width / img.naturalWidth;
+        const yRatio = imgRect.height / img.naturalHeight;
+
+        const markerTop = character.position.top * yRatio - 40; // Adjust the offset as needed
+        const markerLeft = character.position.left * xRatio - 15; // Adjust the offset as needed
+
+        return (
+          <FoundMarker key={index} found={true} position={{ top: markerTop, left: markerLeft }} />
+        );
+      })}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
+// src/components/Home.jsx
 import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Home</h2>
-      <Link to="/game">Play Game</Link>
+      <Link to="/game" className={styles.link}>
+        Play Game
+      </Link>
+      <Link to="/topScores" className={styles.link}>
+        Top Scores
+      </Link>
     </div>
   );
 };
