36fd2cfc6626a8427f6d3da97524edbc225bdfd8
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-31 21:17:29 -0500

    Use root-relative paths for img src

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,15 +7,27 @@ const Sidebar = () => {
       <div className={styles.character_examples}>
         <div>
           <h2 className={styles.character_name}>Waldo</h2>
-          <img src="/public/assets/waldoUp.png" alt="Waldo" className={styles.character_icon} />
+          <img
+            src="/assets/WaldoUp.png" // Corrected path
+            alt="Waldo"
+            className={styles.character_icon}
+          />
         </div>
         <div>
           <h2 className={styles.character_name}>Odlaw</h2>
-          <img src="/public/assets/odlaw.gif" alt="Odlaw" className={styles.character_icon} />
+          <img
+            src="/assets/odlaw.gif" // Corrected path
+            alt="Odlaw"
+            className={styles.character_icon}
+          />
         </div>
         <div>
           <h2 className={styles.character_name}>Wizard</h2>
-          <img src="/public/assets/wizard.gif" alt="Wizard" className={styles.character_icon} />
+          <img
+            src="/assets/wizard.gif" // Corrected path
+            alt="Wizard"
+            className={styles.character_icon}
+          />
         </div>
       </div>
     </div>
